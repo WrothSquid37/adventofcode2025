35c66519067afd7543c587a3a881fbbb1ccf9c1c
Cadeyn Rauscher <cadeynrauscher@gmail.com>
2025-12-07 13:17:36 -0800

    Worked on day 2 part 2

---
+++ day2/part2/advent2/src/App.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class App {
+
+    static ArrayList<String> get_every_nth_chunk (String s, int n) {
+
+        ArrayList<String> tempList = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i += n) {
+            int endIndex = Math.min(s.length(), i+n);
+            tempList.add(s.substring(i, endIndex));
+        }
+
+        return tempList;
+
+    }
+
+    static boolean check_if_nth_duplicates (ArrayList<String> strs, int count) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (String ivs : strs) {
+
+            set.add(ivs);
+
+        }
+
+        int calcCount = strs.size() - set.size();
+
+        return calcCount >= count;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        long count = 0;
+        boolean foundTwo = false;
+
+        File file = new File("src/input.txt");
+
+        String entireTextString = "";
+
+        // Read the file and then close the file (try-with-resources)
+        try (Scanner reader = new Scanner(file)) {
+            entireTextString = reader.next();      
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found...");
+        }
+
+        String[] rangeStr = entireTextString.split(",");
+        for (String unit : rangeStr) {
+
+            String[] indivdualNums = unit.split("-");
+
+            long firstnum = Long.parseLong(indivdualNums[0]);
+            long lastnum = Long.parseLong(indivdualNums[1]);
+
+            for (long i = firstnum; i < lastnum; i++) {
+                
+                String numStr = Long.toString(i);
+
+                for (int c = numStr.length(); c >= 1; c--) {
+
+                    ArrayList<String> splitString = get_every_nth_chunk(numStr, c);
+
+                    if (check_if_nth_duplicates(splitString, 2)) {
+                        count += i;
+                        break;
+                    }
+
+                }
+
+            }
+
+        }
+
+        System.out.println(count);
+
+    }
+}
