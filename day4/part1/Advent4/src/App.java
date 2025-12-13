e9e5202afb35170708a8d4308624a71277c46163
Cadeyn Rauscher <cadeynrauscher@gmail.com>
2025-12-12 19:42:27 -0800

    Fuck

--- day4/part1/Advent4/src/App.java
+++ day4/part1/Advent4/src/App.java
@@ -1,10 +1,53 @@
 import java.util.ArrayList;
 
+import Helper.ExpandGrid;
+import FileHandler.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
-        
+        int forkliftableRolls = 0;
 
+        ArrayList<String> gridUnformatted = FileCustomReader.ReadFileToStringArrayList("src/input.txt");
 
+        ExpandGrid mainGrid = new ExpandGrid(gridUnformatted);  
+        ExpandGrid visualGrid = new ExpandGrid(gridUnformatted);    
+
+        mainGrid.PrintOutGrid();
+
+        for (int x = 0; x < mainGrid.cols; x++) {
+            for (int y = 0; y < mainGrid.rows; y++) {
+                int neighbors = 0;
+                if (mainGrid.getPos(x, y) == '@') {
+                    System.out.println(mainGrid.getPos(x, y));
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                            if (i != 0 && j != 0) {                               
+                                try {
+                                    if (mainGrid.getPos(x+i, y+j) == '@') {
+                                        neighbors++;
+                                    }
+                                }
+                                catch (NullPointerException e) {
+                                    //System.out.println("Nonexistent point");
+                                }   
+                            }
+                        }   
+                    }
+                }
+                System.out.println("Neighbors: " + neighbors);
+                if (neighbors < 4 && mainGrid.getPos(x, y) == '@') {
+                    System.out.println("x: " + x + " y: " + y);
+                    forkliftableRolls++;
+                    visualGrid.ModifyPos(x, y, 'x');
+                    visualGrid.PrintOutGrid();
+                }
+            }
+        }
+        System.out.println("Printing out grid...");
+        visualGrid.PrintOutGrid();
+        System.out.println("Liftable Rolls: " + forkliftableRolls);
+
+        System.out.println("Grid at 0, 3: " + mainGrid.getPos(0, 3));
     }
 }
--- day4/part1/Advent4/src/Helper/ExpandGrid.java
+++ day4/part1/Advent4/src/Helper/ExpandGrid.java
@@ -6,6 +6,8 @@ public class ExpandGrid {
 
     private ArrayList<ArrayList<Character>> grid = new ArrayList<>();
 
+    public int cols = 0, rows = 0;
+
     public ExpandGrid(ArrayList<String> gridStringInput) {
 
         for (String line : gridStringInput) {
@@ -22,10 +24,31 @@ public class ExpandGrid {
 
         }
 
+        cols = grid.get(0).size();
+        rows = grid.size();
+
     }
 
     public Character getPos(int x, int y) {
-        return grid.get(y).get(x);
+        if (x > cols-1 || x < 0 || y > rows-1 || y < 0) return null;
+        return grid.get(x).get(y);
+    }
+
+    public void ModifyPos(int x, int y, char character) {
+        grid.get(x).set(y, character);
+    }   
+
+    public void PrintOutGrid() {
+        for (ArrayList<Character> l : grid) {
+            for (Character c : l) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public void PrintOutDimens() {
+        System.out.println("Cols: " + cols + " Rows: " + rows);
     }
 
 }
