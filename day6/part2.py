70fe363764eadaf2ff0ccf799cd55e47b09706c4
WrothSquid37 <cadeynrauscher@gmail.com>
2025-12-18 19:26:17 -0800

    Checking if everything is pushed

--- day6/part2.py
+++ day6/part2.py
@@ -6,40 +6,20 @@ createdNums = []
 
 with open("input.txt") as f:
     for line in f:
-        unparsedlines.append(line.strip().split())
-        
-    for i in range(len(unparsedlines[0])):
-        a = unparsedlines[0][i]
-        b = unparsedlines[1][i]
-        c = unparsedlines[2][i]
-        d = unparsedlines[3][i]
-        
-        str = ""
-        
-        for ax in range(0, len(a)):
-            str = str + a[ax]
+        unparsedlines.append(line.replace("\n",""))
+    
+ind = 0
 
-        for bx in range(0, len(b)):
-            str = str + b[bx]
-            
-        for cx in range(0, len(c)):
-            str = str + c[cx]
-            
-        for dx in range(0, len(d)):
-            str = str + d[dx]
-        
-        print (str)
-        #print(a, b, c, d)
+while ind < len(unparsedlines):
+    collectedStr
         
         
         
-        op = unparsedlines[4][i];
+       
         
-        # match op:
-        #     case '*':
-        #         total += a * b * c * d
-        #     case '+':
-        #         total += a + b + c + d
+        
+        
+       
     
     print(total)
         
