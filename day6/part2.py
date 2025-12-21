2484d292a75bfa05cd6dca51efd2593f362c4e30
WrothSquid37 <cadeynrauscher@gmail.com>
2025-12-20 16:29:15 -0800

    Hello

--- day6/part2.py
+++ day6/part2.py
@@ -1,32 +1,29 @@
+import math
+
 unparsedlines = []
 nums = []
 total = 0;
 
 createdNums = []
 
-with open("input.txt") as f:
+with open("input2.txt") as f:
     for line in f:
         unparsedlines.append(line.replace("\n",""))
     
 ind = 0
 
-while ind < len(unparsedlines):
-    collectedStr
-        
-        
-        
-       
-        
-        
-        
-       
-    
-    print(total)
-        
-        
+str1 = unparsedlines[0]
+str2 = unparsedlines[1]
+str3 = unparsedlines[2]
+str4 = unparsedlines[3]
+str5 = unparsedlines[4]
 
-    
-    
+while ind < len(unparsedlines[0]):
         
+    unStrNum = str1[ind] + str2[ind] + str3[ind] + str4[ind]
     
+    createdNums.append(unStrNum);
         
+    ind += 1
+    
+print (total)
